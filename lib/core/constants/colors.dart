20783f1b65babc92b5e25b05e49ed68b5c596d13
AzharRizky05 <trinandaazharrizky@gmail.com>
2025-11-11 15:31:40 +0700

    feat(init): [Azhar] Manual setup of project structure, theme constants, and CustomButton widget.

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// Palet Warna Dasar Polinela (Anda bisa ganti nilai Hex-nya nanti)
+const Color kPrimaryColor = Color(0xFF00569E); // Biru Polinela
+const Color kSecondaryColor = Color(0xFFF2A900); // Kuning/Orange Polinela
+const Color kBackgroundColor = Color(0xFFF9F9F9); // Putih Keabu-abuan untuk latar
+const Color kTextColor = Color(0xFF333333); // Warna teks gelap

506a57946a6b09f40bb5607c92cfb7e82341c220
Yona Amalia <yonaamalia30@gmai.com>
2025-11-18 14:37:12 +0700

    skema2: Tambah output Yona pada baris baru di main.php

--- main.php
+++ main.php
@@ -1,6 +1,5 @@
-  GNU nano 8.6                                                                      
-<?php
+  GNU nano 8.6                                                                 <?php
+// Ganti baris ini dengan kode PHP yang ada
 echo "Hello dari Yona";
-
-
+echo "Perubahan Skema 2 oleh Yona Amalia."; // <-- TAMBAH BARIS INI
 ?>
